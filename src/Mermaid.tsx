11df4a1a364519eb7844bc12b6ac657fd9cccb9c
Samuel Wall <oss@samuelwall.co.uk>
2021-08-07 13:29:40 +0000

    style: add eslint

--- src/Mermaid.tsx
+++ src/Mermaid.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, ReactElement } from 'react'
 import mermaid from 'mermaid'
 
 mermaid.initialize({
-    startOnLoad: true,
+  startOnLoad: true
 })
 
 export type MermaidProps = {
@@ -10,8 +10,8 @@ export type MermaidProps = {
 }
 
 export const Mermaid = ({ chart }: MermaidProps): ReactElement<MermaidProps> => {
-    useEffect(() => {
-        mermaid.contentLoaded()
-    }, [])
-    return <div className="mermaid">{chart}</div>
+  useEffect(() => {
+    mermaid.contentLoaded()
+  }, [])
+  return <div className="mermaid">{chart}</div>
 }
--- src/mdxast-mermaid.ts
+++ src/mdxast-mermaid.ts
@@ -6,41 +6,41 @@ type CodeMermaid = Literal<string> & {
   lang: 'mermaid'
 }
 
-function plugin() {
-    return async function transformer(ast: Parent<Node<Data> | Literal, Data>): Promise<Parent> {
-        const instances: [Literal, number, Parent<Node<Data> | Literal, Data>][] = []
-        visit<CodeMermaid>(ast, { type: 'code', lang: 'mermaid' }, (node, index, parent) => {
-          if (typeof parent !== 'undefined') {
-            instances.push([node, index, parent])
-          }
-        })
-        if (!instances.length) {
-            return ast
-        }
-
-        let importFound = false
-        visit(ast, { type: 'import' }, (node: Literal<string>) => {
-            if (/\s*import\s*{\s*Mermaid\s*}\s*from\s*'mdx-mermaid\/Mermaid'\s*;?\s*/.test(node.value)) {
-              importFound = true
-            }
-        })
-        let indexModify = 0
-        if (!importFound) {
-            ast.children.splice(0, 0, {
-                type: 'import',
-                value: 'import { Mermaid } from \'mdx-mermaid/Mermaid\';',
-            })
-            indexModify = 1
-        }
-        instances.map(([node, index, parent]) => {
-            parent.children.splice(index + indexModify, 1, {
-                type: 'jsx',
-                value: `<Mermaid chart={\`${node.value}\`} />`,
-                position: node.position,
-            })
-        })
-        return ast
+function plugin () {
+  return async function transformer (ast: Parent<Node<Data> | Literal, Data>): Promise<Parent> {
+    const instances: [Literal, number, Parent<Node<Data> | Literal, Data>][] = []
+    visit<CodeMermaid>(ast, { type: 'code', lang: 'mermaid' }, (node, index, parent) => {
+      if (typeof parent !== 'undefined') {
+        instances.push([node, index, parent])
+      }
+    })
+    if (!instances.length) {
+      return ast
     }
+
+    let importFound = false
+    visit(ast, { type: 'import' }, (node: Literal<string>) => {
+      if (/\s*import\s*{\s*Mermaid\s*}\s*from\s*'mdx-mermaid\/Mermaid'\s*;?\s*/.test(node.value)) {
+        importFound = true
+      }
+    })
+    let indexModify = 0
+    if (!importFound) {
+      ast.children.splice(0, 0, {
+        type: 'import',
+        value: 'import { Mermaid } from \'mdx-mermaid/Mermaid\';'
+      })
+      indexModify = 1
+    }
+    instances.forEach(([node, index, parent]) => {
+      parent.children.splice(index + indexModify, 1, {
+        type: 'jsx',
+        value: `<Mermaid chart={\`${node.value}\`} />`,
+        position: node.position
+      })
+    })
+    return ast
+  }
 }
 
 export = plugin
